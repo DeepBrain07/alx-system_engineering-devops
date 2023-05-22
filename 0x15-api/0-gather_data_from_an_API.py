f091a16ffaee0eacf6b1b6c8a2114d0f4d4e195e
Timilehin Olusa <olusatimmie07@gmail.com>
2023-05-22 13:11:49 -0700

    completed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,20 +5,21 @@ import sys
 from urllib.request import urlopen
 
 
-link1 = "https://my-json-server.typicode.com/"
-link2 = "DeepBrain07/alx-system_engineering-devops/db"
-link = link1 + link2
-with urlopen(link) as f:
-    """ opens the specified link """
-    data = f.read().decode("UTF-8")
-    data = json.loads(data)
-id = int(sys.argv[1])
-dictLst = data["employees"]
-for i in range(len(dictLst)):
-    if dictLst[i]["id"] == id:
-        name = dictLst[i]["name"]
-        tasks_done = dictLst[i]["tasks_done"]
-        total_tasks = dictLst[i]["total_tasks"]
-        tmp = "Employee {} is done with tasks({}/{}):"
-        print(tmp.format(name, tasks_done, total_tasks))
-        print(dictLst[i]["task_title"])
+if __name__ == "__main__":
+    link1 = "https://my-json-server.typicode.com/"
+    link2 = "DeepBrain07/alx-system_engineering-devops/db"
+    link = link1 + link2
+    with urlopen(link) as f:
+        """ opens the specified link """
+        data = f.read().decode("UTF-8")
+        data = json.loads(data)
+    id = int(sys.argv[1])
+    dictLst = data["employees"]
+    for i in range(len(dictLst)):
+        if dictLst[i]["id"] == id:
+            name = dictLst[i].get("name")
+            tasks_done = dictLst[i].get("tasks_done")
+            total_tasks = dictLst[i].get("total_tasks")
+            tmp = "Employee {} is done with tasks({}/{}):"
+            print(tmp.format(name, tasks_done, total_tasks))
+            print(dictLst[i]["task_title"])
