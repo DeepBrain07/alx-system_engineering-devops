9d83d85becd3b8ae8f3db94509f9953b48ddce70
Timilehin Olusa <olusatimmie07@gmail.com>
2023-06-07 00:18:24 +0100

    Updating

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
                  (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
     headers = {"User-Agent": user_agent}
 
     try:
-        str = f"https://www.reddit.com/r/{subreddit}/about.json"
-        response = requests.get(str, headers=headers)
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        response = requests.get(url, headers=headers)
         return response.json()["data"]["subscribers"]
     except Exception as e:
         return 0
